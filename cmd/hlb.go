6527bdd66323e96a20caf2b46297f1d04581afb9
Luke Jolly <lukejamesjolly@gmail.com>
2025-01-02 16:00:52 -0700

    wip

--- cmd/hlb.go
+++ cmd/hlb.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 	"sync"
 	"syscall"
 
+	// TODO: docs make it seem like it should automatically be imported as cmap
+	cmap "github.com/orcaman/concurrent-map/v2"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -18,7 +21,21 @@ func init() {
 	log.SetLevel(log.WarnLevel)
 }
 
-func loopFilesWorker(wg *sync.WaitGroup, jobs chan string) error {
+// Because of the nature of the program, the newValue should only ever have 1, so appending to existing value is better
+// Possibly test performance in the future
+func updateInodes(exist bool, valueInMap []string, newValue []string) []string {
+	a := make([]string, 0, len(newValue))
+	for _, k := range newValue {
+		a = append(a, k)
+	}
+	// TODO: is there a reason to check for nil?
+	if exist && valueInMap != nil {
+		return append(a, valueInMap...)
+	}
+	return a
+}
+
+func loopFilesWorker(wg *sync.WaitGroup, jobs chan string, inodes cmap.ConcurrentMap[string, []string]) error {
 	for path := range jobs {
 		files, err := os.ReadDir(path)
 		if err != nil {
@@ -40,8 +57,7 @@ func loopFilesWorker(wg *sync.WaitGroup, jobs chan string) error {
 					log.Warn("failed to get syscall.Stat_t")
 					continue
 				}
-				fmt.Println(file.Name())
-				fmt.Printf("stat.Ino = %#v\n", stat.Ino)
+				inodes.Upsert(strconv.FormatUint(stat.Ino, 10), []string{path}, updateInodes)
 			}
 		}
 		wg.Done()
@@ -68,12 +84,16 @@ func LoopDirsFiles(wg *sync.WaitGroup, jobs chan string, path string) error {
 
 func main() {
 	jobs := make(chan string, 100)
+	inodes := cmap.New[[]string]()
 	var wg sync.WaitGroup
 	//Start as many workers you want, now 10 workers
 	for w := 1; w <= 10; w++ {
-		go loopFilesWorker(&wg, jobs)
+		go loopFilesWorker(&wg, jobs, inodes)
 	}
 	//Start the recursion
 	LoopDirsFiles(&wg, jobs, "/media/bigdata")
 	wg.Wait()
+	inodes.IterCb(func(k string, v []string) {
+		fmt.Printf("%s: %d", k, len(v))
+	})
 }
